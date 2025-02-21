5c1751d689ba7a2b47f8a016dd64f7cc36642874
Johannes Jira <Johannes.Jira@uni-bayreuth.de>
2025-02-21 17:09:46 +0100

    feat: add filter

--- frontend/src/taskboard/components/task-table/task-table.component.ts
+++ frontend/src/taskboard/components/task-table/task-table.component.ts
@@ -5,12 +5,14 @@ import {
 import {TaskDto} from '../../client/model/taskDto';
 import {TasksService} from '../../services/tasks.service';
 import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'taskboard-task-table',
   imports: [
     CdkDropList,
-    CdkDrag
+    CdkDrag,
+    FormsModule
   ],
   templateUrl: './task-table.component.html',
   styleUrl: './task-table.component.css'
@@ -20,6 +22,7 @@ export class TaskTableComponent implements OnInit {
   todo: TaskDto[] = [];
   doing: TaskDto[] = [];
   done: TaskDto[] = [];
+  searchString: string = "";
 
   constructor(private tasksService: TasksService) {
     this.tasksService = tasksService;
@@ -57,5 +60,17 @@ export class TaskTableComponent implements OnInit {
 
       this.tasksService.updateTask(movedTask.id, movedTask).then(() => {});
     }
+  }
+
+  filteredTasks(tasks: any[]) {
+    if (!this.searchString) {
+      return tasks;
+    }
+
+    return tasks.filter(task =>
+      task.title.toLowerCase().includes(this.searchString.toLowerCase()) ||
+      task.description.toLowerCase().includes(this.searchString.toLowerCase()) ||
+      (task.assignee?.name && task.assignee.name.toLowerCase().includes(this.searchString.toLowerCase()))
+    );
   }
 }
