2fbdf52e76932ea5ee6e21ede89e15b5f1f44734
Johannes Jira <Johannes.Jira@uni-bayreuth.de>
2025-02-21 12:10:16 +0100

    feat: add drag-and-drop table

--- frontend/src/taskboard/components/task-table/task-table.component.ts
+++ frontend/src/taskboard/components/task-table/task-table.component.ts
@@ -1,29 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {
-  MatCell,
-  MatCellDef,
-  MatColumnDef,
-  MatHeaderCell, MatHeaderCellDef,
-  MatHeaderRow,
-  MatHeaderRowDef,
-  MatRow, MatRowDef, MatTable, MatTableDataSource
+  MatTableDataSource
 } from "@angular/material/table";
 import {TaskDto} from '../../client/model/taskDto';
 import {TasksService} from '../../services/tasks.service';
+import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'taskboard-task-table',
   imports: [
-    MatCell,
-    MatCellDef,
-    MatColumnDef,
-    MatHeaderCell,
-    MatHeaderRow,
-    MatHeaderRowDef,
-    MatRow,
-    MatRowDef,
-    MatTable,
-    MatHeaderCellDef
+    CdkDropList,
+    CdkDrag
   ],
   templateUrl: './task-table.component.html',
   styleUrl: './task-table.component.css'
@@ -31,6 +18,18 @@ import {TasksService} from '../../services/tasks.service';
 export class TaskTableComponent implements OnInit {
   displayedColumns: string[] = ['title', 'description', 'status', 'assignee'];
   dataSource = new MatTableDataSource<TaskDto>([]);
+
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi',
+    'Episode IX – The Rise of Skywalker',
+  ];
 
   constructor(private tasksService: TasksService) {
     this.tasksService = tasksService;
@@ -40,5 +39,9 @@ export class TaskTableComponent implements OnInit {
     this.tasksService.getTasks().then(tasks => {
       this.dataSource.data = tasks;
     });
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
   }
 }
