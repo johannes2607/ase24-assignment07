a0458cfee805a3a66533ba35527e05cb2814d914
Johannes Jira <Johannes.Jira@uni-bayreuth.de>
2025-02-21 15:40:00 +0100

    feat: add more tasks

--- application/src/main/java/de/unibayreuth/se/taskboard/TestFixtures.java
+++ application/src/main/java/de/unibayreuth/se/taskboard/TestFixtures.java
@@ -19,7 +19,24 @@ public class TestFixtures {
     private static final List<Task> TASKS = List.of(
             new Task("Task 1", "Description 1"),
             new Task("Task 2", "Description 2"),
-            new Task("Task 3", "Description 3")
+            new Task("Task 3", "Description 3"),
+            new Task("Task 4", "Description 4"),
+            new Task("Task 5", "Description 5"),
+            new Task("Task 6", "Description 6"),
+            new Task("Task 7", "Description 7"),
+            new Task("Task 8", "Description 8"),
+            new Task("Task 9", "Description 9"),
+            new Task("Task 10", "Description 10"),
+            new Task("Task 11", "Description 11"),
+            new Task("Task 12", "Description 12"),
+            new Task("Task 13", "Description 13"),
+            new Task("Task 14", "Description 14"),
+            new Task("Task 15", "Description 15"),
+            new Task("Task 16", "Description 16"),
+            new Task("Task 17", "Description 17"),
+            new Task("Task 18", "Description 18"),
+            new Task("Task 19", "Description 19"),
+            new Task("Task 20", "Description 20")
     );
 
     public static List<User> getUsers() {
